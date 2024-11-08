dacb65e9f546e5db01bacc6134da8f11c2199d10
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2024-11-08 16:15:30 +0300

    masar day1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,46 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return 'welcome to the MSAR API';
+  }
+
+  @Get('/test')
+  getTest(): string {
+    return 'test2222';
+  }
+
+  @Post('/test')
+  postTest(): string {
+    return 'test';
+  }
+
+  @Put('/test-response')
+  putTestResponse(): any {
+    // or you can remove the "any"
+    return { name: 'ahmad', age: 25 };
+  }
+
+  @Post('/find-sum')
+  findSum(@Query('num1') number1: any, @Query('num2') number2: any): any {
+    // return { number1, number2 };
+    // const sum = number1 + number2; // this will concatenate the numbers since they are strings
+    const sum = Number(number1) + Number(number2); // this will add the numbers
+    return { total: sum };
+  }
+
+  @Post('/find-difference')
+  findDifference(
+    @Query('num1') number1: any,
+    @Query('num2') number2: any,
+  ): any {
+    const difference = Number(number1) - Number(number2);
+    return { 'num1-num2': difference };
+  }
+
+  @Post('/find-product')
+  findProduct(@Query('num1') number1: any, @Query('num2') number2: any): any {
+    const product = Number(number1) * Number(number2);
+    return { 'the numbers multiplied': product };
   }
 }
